4036c4f53e380e9a805ff9fef6b6c52282dabaa8
NayanSP <nayanpatil2502@gmail.com>
2026-04-21 18:06:54 +0530

    Multi lang convertor to english

--- multilang_chat.py
+++ multilang_chat.py
@@ -4,7 +4,15 @@ import streamlit as st
 from dotenv import load_dotenv
 load_dotenv()
 
-prompt = PromptTemplate(template="Your can convert {lang} to english which is typed in english:{ip_text}")
+prompt = PromptTemplate(template="""
+You are an general knowlege and language expert. You have knowledge of all fields. 
+the user provides question in the language selected: {lang}
+the user input is:{ip_text}
+You have to do below task:
+Task 1. Convert it to English Language
+Task 2. provide the details of the query asked in 10 lines
+Task 3. convert the task 2 into same language as user selected: {lang}
+""")
 
 model = ChatOpenAI()
 
@@ -13,7 +21,8 @@ lang_Sel = st.selectbox("Select language you want to written the text in",
                          options=['marathi','hindi','telgu', 'chinese', 'urdu'])
 text = st.text_input("Please enter sentence in any language for conversion")
 
+
 if text:
     st.write(text)
-    res = chain.invoke({'lang': lang_Sel,'ip_text':text})
+    res = chain.invoke({'lang':lang_Sel,'ip_text':text})
     st.write(res.content)
